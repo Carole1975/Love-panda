20cd5eafd8569bdb6d4b66824f0c23f0a18d3652
Carole <carole.velarde@orange.fr>
2017-01-08 21:16:00 +0100

    projet rendu

---
+++ panda.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('button.panda').click(function()
+    {
+        var type= $(this).attr('data-type');
+        var tendance= $(this).attr('data-tendance');
+        if(tendance == "amour"){
+            var debutPhrase = "J'éprouve de l'";
+        }else{
+            var debutPhrase = "J'éprouve de la ";
+        }        var phrase = debutPhrase + tendance + ' pour les pandas ' + type;        
+        $('body').append('<div>' + phrase + '</div>')
+        console.log(phrase);    
+
+    });
+});
+
